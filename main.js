796c71ffd348be43d36910409233b339d0aa079d
hana can <hanacan@hanas-MacBook-Pro.local>
2016-09-15 23:49:51 +0300

    add object part

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
-// GOOD PARTS
+//////////////
+//GOOD PARTS//
+//////////////
 
-document.writeln('helloow')
+document.writeln('helloow');
 
 // THIS IS USED TO DEFINE METHODS IN THIS BOOK!!!
 
 Function.prototype.method = function (name, func) {
-  this.prototype[name]=func;
+  this.prototype[name] = func;
   return this;
-}
+};
 
-console.log('A==="\\u0041"',"A"==="\u0041");
-console.log("'c'+'a'+'t' === 'cat'",'c'+'a'+'t' === 'cat');
+console.log("A==='\\u0041'", "A"==="\u0041");
+console.log("'c' + 'a' + 't' === 'cat'", 'c' + 'a' + 't' === 'cat');
 console.log('false', Boolean(false));
 console.log('undefined', Boolean(undefined));
 console.log("''", Boolean(''));
 console.log(0, Boolean(0));
 console.log(NaN, Boolean(NaN));
 
-// OBJECTS
+///////////
+//OBJECTS//
+///////////
 
 var empty_object = {};
 var stooge = {
-  "first-name": "Jerome",
-  "last-name":" Howard"
+  'first-name': 'Jerome',
+  'last-name': 'Howard'
 };
 
 // objects can nest
@@ -48,7 +52,92 @@ console.log('stoge["middle-name"] ', stooge["middle-name"]);
 console.log('flight.status ',flight.status);
 console.log('stooge["FIRST-NAME"] ', stooge["FIRST-NAME"]);
 
-// CLOSURE
+// fill in default values
+
+var middle =  stooge["middle-name"] || "(none)";
+var status =  flight.status || "unkown";
+
+// inorder not to get type error use &&
+
+console.log('flight.equipment ', flight.equipment);
+//console.log('flight.equipment.model ', flight.equipment.model);
+console.log(flight.equipment && flight.equipment.model); //doesnt throw error but returns undefined
+
+// update
+stooge['first-name'] = 'Hana';
+stooge['middle-name'] = 'Can';
+stooge.nickname = 'watermelon';
+flight.equipment = {
+  model: 'Boeing 777'
+};
+flight.status = 'overdue';
+
+// objects are passed around by reference
+var x = stooge;
+x.nickname = 'watermelon';
+var nick = stooge.nickname;
+console.log(nick); //watermelon
+
+var a = {}, b = {}, c = {}; //each refer to different empty obj
+a = b = c = {}; //each refer to the same empty obj
+
+// prototype
+
+// all objects are linked to Object.prototype
+
+if (typeof Object.create !== 'function'){
+  Object.create = function(o){
+    var F = function(){};
+    F.prototype = o;
+    return new F();
+  };
+}
+
+var another_stooge = Object.create(stooge);
+
+another_stooge['first-name'] = 'Burak';
+another_stooge['middle-name'] = 'Can';
+another_stooge.nickname = 'melon';
+
+stooge.profession = 'developer';
+
+// reflection
+console.log('typeof flight.number ', typeof flight.number);
+console.log('typeof flight.status ', typeof flight.status);
+console.log('typeof flight.arrival ', typeof flight.arrival);
+console.log('typeof flight.manifest', typeof flight.manifest);
+
+console.log('typeof flight.toString', typeof flight.toString);
+console.log('typeof flight.constructor', typeof flight.constructor);
+
+console.log('flight.hasOwnProperty("number") ',flight.hasOwnProperty('number'));
+console.log('flight.hasOwnProperty("constructor")', flight.hasOwnProperty('constructor'));
+
+// enumeration
+var name;
+for (name in another_stooge) {
+  if(typeof another_stooge[name] !== 'function'){
+    document.writeln(name + ': ' + another_stooge[name]);
+  }
+}
+// to assure the order of the names
+var i;
+var properties = [
+  'first-name',
+  'middle-name',
+  'last-name',
+  'profession'
+];
+
+for (i = 0; i < properties.length; i += 1){
+  document.writeln(properties[i] + ': ' +
+          another_stooge[properties[i]]);
+}
+
+///////////
+//CLOSURE//
+///////////
+
 var myObject = function(){
   var value = 0;
 
@@ -90,7 +179,7 @@ var fade = function(node){
   };
   setTimeout(step, 100);
 };
-fade(document.body)
+fade(document.body);
 
 // bad example
 //on click on li element returns length but not i
@@ -122,9 +211,12 @@ var add_the_good_handlers = function(nodes) {
   }
 };
 
-add_the_good_handlers(document.getElementsByTagName('li'))
+add_the_good_handlers(document.getElementsByTagName('li'));
 
-// CALLBACKS
+/////////////
+//CALLBACKS//
+/////////////
+
 // make async req. providing  a callback func.
 // the func. will be invoked when the server response is received
 
